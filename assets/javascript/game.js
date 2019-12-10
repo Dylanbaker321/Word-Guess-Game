b8f03b47c458cc61bc635772915f7f2fa88ced97
Dylan Baker <Dylanbaker321@gmail.com>
2019-12-10 14:09:59 -0700

    Collects Key Clicks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,12 +3,25 @@ var words = ["velociraptor", "triceratops", "trex", "stegosaurus", "iguanodon", 
   "brontosaurus", "megalodon"];
 
   //selceted word
-
   var gameWord = "";
-
+//Creating the function that runs when the page loads and starts the game
   function game() {
     gameWord = words[Math.floor(Math.random() * words.length)];
     console.log(gameWord);
   };
 
+  //collects key clicksS
+  document.onkeyup = function(event) {
+    if (event.keyCode >= 65 && event.keyCode <= 90) { 
+      var guess = event.key.toLowerCase();
+   
+      
+      //Checks for what is guesses
+      console.log(guess);
+   
+    }
+  };
+
+
+  // Runs the main functio
   game();
